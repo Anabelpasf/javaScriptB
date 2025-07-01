2da2bfdfd7df0b2d845e285e5bb8113ae6cef0c9
Anabelpasf <anavitoriapascoafranco@gmail.com>
2025-07-01 12:25:52 -0300

    aula de javaScript 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,26 @@
+var global = "variavel global"
+console.log(global)
+
+function exemplo2(){
+    var local = "variavel local";
+    console.log(local);
+}
+
+exemplo2()
+//  console.log(local);
+
+let x = 10;
+if(x > 5 ){
+    let y = 20;
+    console.log("valor de x: ", y )
+}
+
+console.log(x);
+console.log(y);
+
+const_constante = "Texto"
+console.log(_constante)
+
+function saudacao(){
+    alert("Bem vindexs ao nosso site 3 more")
+}
