0a72d7d251ea545c8c697ec3fad61e6d2610fee5
Anabel <anavitoriapascoafranco@gmail.com>
2025-07-08 12:33:41 -0300

    08/07

---
+++ exercicios/script.js
@@ -0,0 +1,10 @@
+function somar(){
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+
+    document.getElementById("Resultado1").innerText = "Resultado:  " + (n1 + n2);
+}
+
+function verificar(){
+    let idade = Number(document.getElementById("idade"))
+}
